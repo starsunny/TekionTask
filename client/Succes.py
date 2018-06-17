5e52f8f36f4db4a20c4149065b756009df820196
Sunny kalal <starsunny@users.noreply.github.com>
2018-06-17 20:36:00 +0530

    Update Succes.py

--- client/Succes.py
+++ client/Succes.py
@@ -12,7 +12,6 @@ IPa=s[6]
 Usr=s[7]
 
 def Success(client) :
-                #client.send("dialog --msgbox \"      IP \n" + IPa + "\n \" Times \" \n" + count1 + "\n \" User \"\n" + Usr + "\n \"   20 50")
-                #client.send("dialog --msgbox \" IP \n" + IPa + "\n \n" + count1 + "\n \n" + Usr + "\n \"   20 50")
                 client.send("dialog --msgbox \"How many times?\n" + count1 + "\nWhich User?\n" + Usr + "\n  \"   10 50")
-                #client.send("dialog --msgbox \"    Count      IP    User\n" + result + "\n \"   10 50")
+                #client.send("dialog --msgbox \"    Details \n" + result + "\n \"   10 50")
+                #uncomment this last line and comment another line to get all the user details.
